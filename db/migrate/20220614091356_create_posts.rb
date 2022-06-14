7f26078038ab69a1de31ee6bc054d25e336c6278
aokkkky062 <mcyzbnos062@docomo.ne.jp>
2022-06-15 03:02:06 +0900

    Revert "マイグレーションファイルの追記"
    This reverts commit cb04dfad7311b6d929edced896f1ca9720dfe0dc.

--- db/migrate/20220614091356_create_posts.rb
+++ db/migrate/20220614091356_create_posts.rb
@@ -1,10 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :name,         null: false
+      t.string :name, null: false
       t.string :date
-      t.text   :program,      null: false
-      t.text   :information,  null: false
+      t.text   :program
+      t.text   :information
       t.timestamps
     end
   end
