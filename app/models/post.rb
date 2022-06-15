694b362c86c44c68618c1f34f9c58ffcd97cf054
aokkkky062 <mcyzbnos062@docomo.ne.jp>
2022-06-16 02:19:37 +0900

    バリデーションを追記
    「dateカラムは必須」というバリデーションを設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
 
+  validates :date,        presence: true
   validates :program,     presence: true
   validates :information, presence: true
 end
