d5d717960e8b75dc1951352fe13b7e6944457757
aokkkky062 <mcyzbnos062@docomo.ne.jp>
2022-06-16 18:35:02 +0900

    マイグレーションファイルの修正
    「date」カラムを入力必須にする

--- db/migrate/20220614091356_create_posts.rb
+++ db/migrate/20220614091356_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.date   :date
+      t.date   :date,         null: false
       t.text   :program,      null: false
       t.text   :information,  null: false
       t.text   :feedback
