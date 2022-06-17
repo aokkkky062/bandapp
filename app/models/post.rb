44f6185028a700e0a17bf8e073d12ab4f397677e
aokkkky062 <mcyzbnos062@docomo.ne.jp>
2022-06-17 15:43:52 +0900

    練習報告詳細画面にコメント機能を実装
    ・コメント機能の実装
    ・コメントが即時に反応する機能を実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many   :report_comments
 
   validates :date,        presence: true
   validates :program,     presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_14_091356) do
+ActiveRecord::Schema.define(version: 2022_06_17_050844) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.date "date"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2022_06_14_091356) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "report_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
